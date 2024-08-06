5f1d39745e7da8e9407e7768e7758044eb434801
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-06 14:17:08 +0300

    move header.component to folder header. Create user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
