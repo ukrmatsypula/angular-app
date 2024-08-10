c5e9e064d2754070af2ae8c3f58d4d458b8a8557
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-10 17:45:36 +0300

    add task component and render it

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {}
