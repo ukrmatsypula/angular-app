82ee4860ff935ae0e1933b66ada681596eb287c7
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-13 13:33:01 +0300

    render selected task data

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
@@ -7,4 +15,6 @@ import { Component } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
-export class TaskComponent {}
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+}
