a4e05ddf514eae5ae9268e24c0f674fac8ab6caf
Roman Matsypula <r.matsypula@letyshops.com>
2024-08-16 00:54:02 +0300

    add new task fields

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,26 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  constructor() {
+    console.log(this.enteredTitle);
+    console.log(this.enteredSummary);
+    console.log(this.enteredDate);
   }
 }
